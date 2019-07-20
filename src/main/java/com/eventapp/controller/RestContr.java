dd950cdfe7f983b14acf23cefee82e6cec30dbe9
antek <antonimaruniewicz@gmail.com>
2019-07-20 14:54:24 +0200

    + restController update + EventService + repos

--- src/main/java/com/eventapp/controller/RestContr.java
+++ src/main/java/com/eventapp/controller/RestContr.java
@@ -3,6 +3,8 @@ package com.eventapp.controller;
 import com.eventapp.models.CommonEvent;
 import com.eventapp.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,34 +24,36 @@ public class RestContr {
     public RestContr(EventService eventService) {
         this.eventService = eventService;
     }
+
+    @EventListener(ApplicationReadyEvent.class)
     @GetMapping("/findAll")
     @ResponseStatus(HttpStatus.FOUND)
     public Iterable<CommonEvent> findAll() {
-        return eventService.findAll();
+        return (Iterable<CommonEvent>) eventService.findAll();
     }
     @GetMapping("findByName")
     @ResponseStatus(HttpStatus.FOUND)
     public Optional<CommonEvent> findByName(@RequestParam String name) {
-        return Optional.ofNullable(eventService.findByName());
+        return Optional.ofNullable(eventService.findByName(name));
     }
     @GetMapping("/findByCity")
     @ResponseStatus(HttpStatus.FOUND)
     public Iterable<CommonEvent> findByCity(@RequestParam String city) {
-        return eventService.findByCity();
+        return eventService.findByCity(city);
     }
     @GetMapping("/findByCountry")
     @ResponseStatus(HttpStatus.FOUND)
     public Iterable<CommonEvent> findByCountry(@RequestParam String country) {
-        return eventService.findByCountry();
+        return eventService.findByCountry(country);
     }
     @GetMapping("findByStartDate")
     @ResponseStatus(HttpStatus.FOUND)
     public Iterable<CommonEvent> findByStartDate(@RequestParam LocalDateTime startDate) {
-        return eventService.findByStartDate();
+        return eventService.findByStartDate(startDate);
     }
     @GetMapping("findByEndDate")
     @ResponseStatus(HttpStatus.FOUND)
-    public Iterable<CommonEvent> findByStartDate(@RequestParam LocalDateTime endDate) {
-        return eventService.findByEndDate();
+    public Iterable<CommonEvent> findByEndDate(@RequestParam LocalDateTime endDate) {
+        return eventService.findByEndDate(endDate);
     }
 }
